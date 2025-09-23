bac8123e79cdc721c1a888fe9b8527810490e9d7
jose steban <a01723866@tec.mx>
2025-09-23 13:33:38 -0600

    arreglo url canonica pt2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.santreindustrial.com',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
